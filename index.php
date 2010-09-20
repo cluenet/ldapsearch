becdd21bcfb145edeb3125e132915281fca9ceb0
Mantas Mikulėnas <grawity@nullroute.eu.org>
2010-09-20 22:22:35 +0300

    Use a stricter base DN

--- index.php
+++ index.php
@@ -148,7 +148,7 @@ do if ($Query) {
 	// and get results
 	$ldapConn = ldap_connect_and_do_things();
 	if ($ldapConn) {
-		$ldapSH = @ldap_search($ldapConn, BASE_DN, $Filter, Array(), null, 0);
+		$ldapSH = @ldap_search($ldapConn, "ou=people,".BASE_DN, $Filter, Array(), null, 0);
 		if ($ldapSH === false) {
 			$SysErrors[] = "LDAP query failed.";
 			$NumResults = 0;
